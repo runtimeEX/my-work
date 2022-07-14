37ff44a22f1c53ca0d212a60bf2db98a5b17668f
zhouhao <zhou.hao@vpclub.cn>
2022-07-14 16:10:57 +0800

    init

---
+++ springboot-sharding/src/main/java/com/sharding/SpringbootShardingApplication.java
@@ -0,0 +1,19 @@
+package com.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : zhouhao
+ * @version V1.0
+ * @Project: springboot-sharding
+ * @Package com.sharding
+ * @Description: TODO
+ * @date Date : 2022年07月14日 下午3:55
+ */
+@SpringBootApplication
+public class SpringbootShardingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShardingApplication.class, args);
+    }
+}
